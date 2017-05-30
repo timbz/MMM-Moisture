941cae24d207dbb818c805e462553ab999a0ed0e
timbz <timon.baetz@gmail.com>
2017-05-30 19:50:22 +0200

    Added endpoint

--- MMM-Moisture.js
+++ MMM-Moisture.js
@@ -8,7 +8,7 @@ Module.register("MMM-Moisture", {
 
   // Default module config.
   defaults: {
-    endpoint: 0.5,
+    endpoint: 'http://hass.home.lan/api/states/sensor.palme_moisture',
     threshold: 35,
     updateInterval: 10 * 60 * 1000 // every 10 minutes
   },
