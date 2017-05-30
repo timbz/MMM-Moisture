9bdc9fdc24cf5a96a849f2e4d9c55182ec2daca9
timbz <timon.baetz@gmail.com>
2017-05-30 20:16:36 +0200

    More logging

--- MMM-Moisture.js
+++ MMM-Moisture.js
@@ -34,6 +34,7 @@ Module.register("MMM-Moisture", {
   },
 
   updateSensor: function () {
+    Log.info(self.name + ": Getting sensor data.");
     var self = this;
     var sensorRequest = new XMLHttpRequest();
     sensorRequest.open("GET", this.config.endpoint, true);
